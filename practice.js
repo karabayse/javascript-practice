9c393d9bf885d6efa9561661b717f76e4613c3fc
Kara Bayse <karabayse@gmail.com>
2018-11-05 15:33:42 -0600

    return month as a string function

--- practice.js
+++ practice.js
@@ -37,3 +37,47 @@ function findDigitAmount(num) {
 function findLargestNum(arr) {
   return Math.max.apply(Math, arr);
 }
+
+// Create a function that takes a number (from 1 to 12) and
+// return its corresponding month name as a string
+function month_name(num) {
+	if (num === 1) {
+		return 'January';
+	}
+	else if (num === 2) {
+		return 'February';
+	}
+	else if (num === 3) {
+		return 'March';
+	}
+	else if (num === 4) {
+		return 'April';
+	}
+	else if (num === 5) {
+		return 'May';
+	}
+	else if (num === 6) {
+		return 'June';
+	}
+	else if (num === 7) {
+		return 'July';
+	}
+	else if (num === 8) {
+		return 'August';
+	}
+	else if (num === 9) {
+		return 'September';
+	}
+	else if (num === 10) {
+		return 'October';
+	}
+	else if (num === 11) {
+		return 'November';
+	}
+	else if (num === 12) {
+		return 'December';
+	}
+	else {
+		return null;
+	}
+}
