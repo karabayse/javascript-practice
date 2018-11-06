816566540bfbb176b2a458c850ddda23fff7a157
Kara Bayse <karabayse@gmail.com>
2018-11-06 07:39:28 -0600

    take a string and return the word count

--- practice.js
+++ practice.js
@@ -101,3 +101,9 @@ function findSmallestNum(arr) {
 function reverse(str) {
 	return str.split("").reverse().join("");
 }
+
+// Create a function that takes a string and returns the word count
+// The string will be a sentence.
+function countWords(str) {
+	return str.split(" ").length;
+}
